4a656e9d24dedbe1a778740b30dc574fe1cc96d3
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-02-10 19:48:20 +0200

    next pers fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { reducer } from './reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'articles',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
+
 // import { configureStore } from '@reduxjs/toolkit';
 // import { reducer } from './reducer';
 // import {
@@ -20,36 +53,3 @@
 //     }),
 // });
 // export const persistor = persistStore(store);
-
-import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { reducer } from './reducer';
-
-const persistConfig = {
-  key: 'articles',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
-export const persistor = persistStore(store);
